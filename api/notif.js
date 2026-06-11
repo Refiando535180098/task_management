4c77647e35aa8d42ae559f931eb3151737816979
Refiando <refihatake@gmail.com>
2026-06-12 04:23:35 +0700

    tambah backend api

--- api/notif.js
+++ api/notif.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
         // GANTI INI DENGAN REST API KEY YANG ASLI (LIHAT LANGKAH 3)
-        'Authorization': 'Basic os_v2_app_6g3tdf7fvzgdla4cffwxevwydz2aonqqw2auwinl6izhgueslnblwsib547gqdsqzn5cd5ircbb2nkvovya3zd5iujb5zlrusgvflxa'
+        'Authorization': 'Basic os_v2_app_6g3tdf7fvzgdla4cffwxevwyd3vl2fb6tgrekhnmj3kwm6sxyfbzspzzylttt4algcllsetklq5hjr2msfhjhvoe7r6blgq7ai3pvdy'
       },
       body: JSON.stringify({
         app_id: "f1b73197-e5ae-4c35-8382-296d7256d81e",
